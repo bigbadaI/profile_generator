b6dcdd765248c444c0514233f8da6b97cb72a5c0
BigBadAI <flashmefusion@hotmail.ca>
2021-01-12 22:38:12 +0000

    fixed some spelling errors

--- survey.js
+++ survey.js
@@ -13,7 +13,7 @@ let ans6 = "";
 let ans7 = "";
 rl.question('What\'s your name? ', (answer) => {
   ans1 = answer;
-  rl.question(`Nice to meet you, ${ans1} \nLet's make a profile together. \nWhat's an activity enjoy?`, (answer) => {
+  rl.question(`Nice to meet you, ${ans1} \nLet's make a profile together. \nWhat's an activity enjoy? `, (answer) => {
     ans2 = answer;
     rl.question(`While your enjoying your day ${ans2}, what do you like to listen to? `, (answer) => {
       ans3 = answer;
@@ -26,7 +26,7 @@ rl.question('What\'s your name? ', (answer) => {
             rl.question(`Lastly, What is your super power and in a few words why are you so amazing at it? `, (answer) => {
               ans7 = answer;
               
-              console.log(`Thank you for answering my questions, I think your profile should read.\nHi there, my name is ${ans1}\nI think the best time to get to know me is over ${ans4} while I devour my plate of ${ans5} (sorry it's my favorite)\nAny day that I can spend ${ans2} while listening to ${ans3} is a good day in my books.\n"Theatres" NAH let's maybe check out my favorite sport ${ans6}\nLet's finish this off with a banger, my super power is ${ans7}!`);
+              console.log(`Thank you for answering my questions, I think your profile should read.\nHi there, my name is ${ans1}\nI think the best time to get to know me is during ${ans4} while I devour my plate of ${ans5} (sorry it's my favorite)\nAny day that I can spend ${ans2} while listening to ${ans3} is a good day in my books.\n"Theatres" NAH let's maybe check out my favorite sport ${ans6}\nLet's finish this off with a banger, my super power is ${ans7}!`);
               
               rl.close();
             });
